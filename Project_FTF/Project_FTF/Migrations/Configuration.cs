a83aceea183bfe193b7c85419b8ac94c7af95c04
WINIFRED  TIMMINS <11341061@IT.NUIGALWAY.IE>
2016-04-14 16:27:27 +0100

    Updates

--- Project_FTF/Project_FTF/Migrations/Configuration.cs
+++ Project_FTF/Project_FTF/Migrations/Configuration.cs
@@ -46,15 +46,28 @@ namespace Project_FTF.Migrations
                 new Item{Status = "Lost", FirstName = "Freddie", LastName = "Timmins", EmailAddress = "freddie@hotmail.com", ItemType = "Phone", ItemDesc="Black iPhone", Location = "College Bar, NUI Galway, Newcastle, Galway"},
                 new Item{Status = "Found", FirstName = "Colm", LastName = "Quish", EmailAddress = "colm@hotmail.com", ItemType = "Wallet", ItemDesc="Brown leather wallet", Location = "IT 312, IT Department, NUI Galway, Newcastle, Galway"},
                 new Item{Status = "Found", FirstName = "JD", LastName = "Kiely", EmailAddress = "jd@hotmail.com", ItemType = "Keys", ItemDesc="Volkswagon car keys", Location = "Kingfisher Gym, NUI Galway, Newcastle, Galway"},
-                new Item{Status = "Lost", FirstName = "Ulrich", LastName = "Lunde", EmailAddress = "ulrich@hotmail.com", ItemType = "Jewellery", ItemDesc="Silver tennis bracelet", Location = "Smokeys, NUI Galway, Newcastle, Galway"},            
+                new Item{Status = "Lost", FirstName = "Ulrich", LastName = "Lunde", EmailAddress = "ulrich@hotmail.com", ItemType = "Jewellery", ItemDesc="Silver tennis bracelet", Location = "Smokeys, NUI Galway, Newcastle, Galway"}, 
+                new Item{Status = "Lost", FirstName = "Patrick", LastName = "Glynn", EmailAddress = "patrick@hotmail.com", ItemType = "Phone", ItemDesc="Samsung Galaxy s6, screen saver of my dog lilo.", Location = "College Bar, NUI Galway, Newcastle, Galway"},
+                new Item{Status = "Found", FirstName = "John", LastName = "Hanbury", EmailAddress = "john@hotmail.com", ItemType = "Sports Equipment", ItemDesc="Large white cooper helment, lost on wednesday 13.4.16", Location = "Dangan Sports Grounds, Dangan, Galway"},
+                new Item{Status = "Found", FirstName = "Caoimhe", LastName = "", EmailAddress = "caoimhe@hotmail.com", ItemType = "Wallet", ItemDesc="Brown Michael Kors wallet, identification inside", Location = "College Bar, NUI Galway, Newcastle, Galway"},
+                new Item{Status = "Lost", FirstName = "Tara", LastName = "Grealish", EmailAddress = "tara@hotmail.com", ItemType = "Sports Equipment", ItemDesc="Nike roshe runs, ladies, size 5", Location = "Kingfisher Gym, NUI Galway, Newcastle, Galway"},
+                new Item{Status = "Lost", FirstName = "Mark", LastName = "O'Rourke", EmailAddress = "mark@hotmail.com", ItemType = "Sports Equipment", ItemDesc="Squash racket with my name engraved on it and the date 16.3.93", Location = "Kingfisher Gym, NUI Galway, Newcastle, Galway"},
+            
             };
+
             items.ForEach(s => context.Items.AddOrUpdate(p => p.EmailAddress, s));
             context.SaveChanges();
                        
             context.Contacts.AddOrUpdate(u => u.UserName,
                 new Contact { UserName = "FreddieTim", EmailAddress = "freddie_@hotmail.com" },
                 new Contact { UserName = "ColmQ", EmailAddress = "colm_@hotmail.com" },
-                new Contact { UserName = "JDk", EmailAddress = "jd_@hotmail.com" }
+                new Contact { UserName = "JDk", EmailAddress = "jd_@hotmail.com" },
+                new Contact { UserName = "Ulrich", EmailAddress = "ulirch@hotmail.com" },
+                new Contact { UserName = "Pajo", EmailAddress = "patrick@hotmail.com" },
+                new Contact { UserName = "Johnboi", EmailAddress = "john@hotmail.com" },
+                new Contact { UserName = "Caoimhs", EmailAddress = "caoimhe@hotmail.com" },
+                new Contact { UserName = "Tazz", EmailAddress = "tara@hotmail.com" },
+                new Contact { UserName = "Marky", EmailAddress = "mark@hotmail.com" }
                 );
             
             
---
+++ Project_FTF/UnitTests/ItemControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project_FTF.Models;
+using Project_FTF.DAL;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ItemControllerTests
+    {
+        [TestMethod]
+        public void CreateMethod()
+        {
+            Project_FTF.Controllers.ItemController itemTest = new Project_FTF.Controllers.ItemController();
+            itemTest.Create(new Item());         
+           
+        }
+
+        [TestMethod]
+        public void EditMethod()
+        {
+            Project_FTF.Controllers.ItemController itemTest2 = new Project_FTF.Controllers.ItemController();
+            itemTest2.Edit();
+
+        }
+            
+        [TestMethod]
+        public void DetailsMethod()
+        {
+            Project_FTF.Controllers.ItemController itemTest3 = new Project_FTF.Controllers.ItemController();
+            itemTest3.Details(Item);
+
+        }
+      
+    }
+}
